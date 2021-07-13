4499ed8c502b0395710599394ddd7f5c5693f477
Akbar Umar Alfaroq <akbarumar88@gmail.com>
2021-07-14 00:21:17 +0700

    Validasi email selain id-nya sendiri

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -36,8 +36,8 @@ router.post("/register", validate(), handlerInput, async function (req, res) {
 });
 
 router.post("/email", async function (req, res, next) {
-  let sql = `SELECT * FROM users where email=$1 `;
-  let data = [req.body.email];
+  let sql = `SELECT * FROM users where email=$1 AND id!=$2`;
+  let data = [req.body.email, req.body.id];
   let result = await koneksi.any(sql, data);
   if (result.length == 0) {
     res.json({
